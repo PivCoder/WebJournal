9d968d0207e1d73a26b0e9bbd513114bd8bf6409
Литовченко Матвей Дмитриевич <200288@edu.psuti.ru>
2022-10-24 13:51:29 +0400

    New Spring files

---
+++ src/main/java/com/example/webjournalspring_v2/WebJournalSpringV2Application.java
@@ -0,0 +1,13 @@
+package com.example.webjournalspring_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJournalSpringV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebJournalSpringV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webjournalspring_v2/WebJournalSpringV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webjournalspring_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJournalSpringV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
