d6a024bdcf57366c8c152ed47ba2bcd2be05441d
Литовченко Матвей Дмитриевич <200288@edu.psuti.ru>
2022-11-21 15:34:56 +0400

    Refactor home page

--- src/main/java/com/example/webjournalspring_v2/AutorizationController.java
+++ src/main/java/com/example/webjournalspring_v2/AutorizationController.java
@@ -1,4 +1,14 @@
 package com.example.webjournalspring_v2;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/autorization")
 public class AutorizationController {
+    @GetMapping
+    public String showArticlesForm() {
+        return "autorization";
+    }
 }
